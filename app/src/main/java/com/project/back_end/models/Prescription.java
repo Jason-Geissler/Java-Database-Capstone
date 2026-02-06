5624028a537f59d0006e8aa686d5f3642b6355a1
Jason-Geissler <jasonbgeissler@gmail.com>
2026-02-06 16:31:06 -0600

    Update Prescription.java

--- app/src/main/java/com/project/back_end/models/Prescription.java
+++ app/src/main/java/com/project/back_end/models/Prescription.java
@@ -1,4 +1,4 @@
-package com.smartclinic.model;
+package com.project.back_end.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
